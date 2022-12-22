656c6c73d08b181b66efaed84c2f90e170a90197
Matti Christmann <115004154+matchristmann@users.noreply.github.com>
2022-12-22 13:38:52 +0100

    Update timberServiceConditions.py

--- RFEM/TypesForTimberDesign/timberServiceConditions.py
+++ RFEM/TypesForTimberDesign/timberServiceConditions.py
@@ -14,9 +14,19 @@ class TimberServiceConditions():
                 moisture_service_condition = TimberServiceConditionsMoistureClass.TIMBER_MOISTURE_SERVICE_CONDITION_TYPE_DRY,
                 treatment = TimberServiceConditionsTreatmentClass.TREATMENT_TYPE_NONE,
                 temperature = TimberServiceConditionsTemperatureClass.TEMPERATURE_TYPE_EQUAL_TO_50,
-                member_pressure_treated: (bool) = True,
+                service_conditions: dict = {"outdoor_environment": False,
+                                        "long_term_high_temperature_of_surface": False,
+                                        "permanent_load_design_situation": False,
+                                        "timber_structures": False,
+                                        "short_term_construction_or_maintenance": False,
+                                        "timber_is_point_impregnated": False,
+                                        "member_pressure_treated": False,
+                                        "equilibrium_moisture_content": None,
+                                        "user_defined_temperature": None,
+                                        "impregnation_with_flame_retardant_under_pressure": False},
                 comment: str = '',
-                params: dict = None):
+                params: dict = None,
+                model = Model):
         """
         Args:
             no (int): Timber Member Shear Panel Tag
@@ -28,12 +38,24 @@ class TimberServiceConditions():
             service_class (enum): Timber Service Conditions Moisture Class
             treatment (enum): Timber service conditions treatment class
             temperature (enum): Timber service conditions temperature class
+            service_conditions (dict, optional): Dictionary containing additional settings for service conditions
+                                        {outdoor_environment (bool): enable/disable,
+                                        long_term_high_temperature (bool): enable/disable,
+                                        permanent_load_design_situation (bool): enable/disable,
+                                        timber_temporary_structures (bool): enable/disable,
+                                        short_term_construction_or_maintenance (bool): enable/disable,
+                                        timber_is_point_impregnated (bool): enable/disable,
+                                        member_pressure_treated (bool): enable/disable,
+                                        equilibrium_moisture_content (float): Set equilibrium of moisture content,
+                                        user_defined_temperature (float): Set a user defined temperature,
+                                        impregnation_with_flame_retardant_under_pressure (bool): enable/disable}
             comment (str, optional): Comment
             params (dict, optional): Any WS Parameter relevant to the object and its value in form of a dictionary
+            model (RFEM Class, optional): Model to be edited
         """
 
          # Client Model | Types For Timber Design Service Conditions
-        clientObject = Model.clientModel.factory.create('ns0:timber_service_class')
+        clientObject = model.clientModel.factory.create('ns0:timber_service_conditions')
 
         # Clears object atributes | Sets all atributes to None
         clearAttributes(clientObject)
@@ -64,9 +86,10 @@ class TimberServiceConditions():
         if temperature:
             clientObject.temperature = temperature.name
 
-        # Setting treatment of timber
-        if member_pressure_treated:
-            clientObject.member_pressure_treatment = member_pressure_treated
+        # Setting additional settings for service conditions
+        if service_conditions:
+            for key in service_conditions:
+                clientObject[key] = service_conditions[key]
 
         # User Defined Name
         if name:
@@ -85,4 +108,4 @@ class TimberServiceConditions():
         deleteEmptyAttributes(clientObject)
 
         # Add Service Class to client model
-        Model.clientModel.service.set_timber_service_conditions(clientObject)
+        model.clientModel.service.set_timber_service_conditions(clientObject)
